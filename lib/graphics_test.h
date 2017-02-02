581c2673e9e4374b52c88911e4f0a97ebb0a9e39
chadlagore <chad.d.lagore@gmail.com>
2017-02-01 23:19:09 -0800

    a first stab - probly some bugs :bug:

---
+++ lib/graphics_test.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "graphics.h"
+
+#include "OutGraphicsCharFont2.c"
+
+void TestHLine(int x1, int y1, int length, int Colour);
+void TestVLine(int x1, int y1, int length, int Colour);
+void TestLine(int x1, int y1, int x2, int y2, int Colour);
+void TestFilledRectangle(int x1, int y1, int x2, int y2, int color);
+void TestCircle(int x0, int y0, int radius, int color);
+void line_test_screen();
