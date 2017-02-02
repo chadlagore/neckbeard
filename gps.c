5daf1d7b03670402ffaa8878049753c8e5780cee
georgiajoy <georgia.iredale@gmail.com>
2017-02-01 20:40:12 -0800

    GPS stuff done

--- gps.c
+++ gps.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <time.h>
+#include "altera_up_avalon_character_lcd.h"
 
 char packetStr[82]; //Max length of message
 
@@ -89,26 +91,43 @@ void parse_packet(){
 	EW_indicator = tokens[5];
 	satelites_used = tokens [7];
 	checksum = tokens[14];
+
+	/* Shift commas left by two in lat and long */
+	char upper = latitude[2];
+	char lower = latitude[3];
+	latitude[2] = '.';
+	latitude[3] = upper;
+	latitude[4] = lower;
+
+	upper = longitude[3];
+	lower = longitude[4];
+	longitude[3] = '.';
+	longitude[4] = upper;
+	longitude[5] = lower;
 }
 
 /*
  * Covert UTC timestamp to local time
  */
 void utc_to_local() {
-	char *temp = utc_time;
-	int hours_h = (int) *temp;
-	int hours_l = (int) *temp+1;
-	int minutes_h = (int) *temp +2;
-	int minutes_l = (int) *temp +3;
-	int seconds_h = (int) *temp +4;
-	int seconds_l = (int) *temp +5;
+	int hours_h = ((char)utc_time[0]) - '0';
+	int hours_l = ((char)utc_time[1]) - '0';
+	int minutes_h = ((char)utc_time[2]) - '0';
+	int minutes_l = ((char)utc_time[3]) - '0';
+	int seconds_h = ((char)utc_time[4]) - '0';
+	int seconds_l = ((char)utc_time[5]) - '0';
 
 	/* Add 8 hours to UTC time for Vancouver time */
-	int hours = (hours_h*10 + hours_l + 8) % 24;
+	int hours = (hours_h*10 + hours_l + 4) % 24;
 
 	hours_h = hours / 10;
 	hours_l = hours % 10;
-	printf("%d%d:%d%d:%d%d\n", hours_h, hours_l, minutes_h, minutes_l, seconds_h, seconds_l);
+	printf("%d%d : %d%d : %d%d\n", hours_h, hours_l, minutes_h, minutes_l, seconds_h, seconds_l);
+
+	/* Write the values to the hex displays */
+	*HEX4_5 = (hours_h << 4) | hours_l;
+	*HEX2_3 = (minutes_h << 4) | minutes_l;
+	*HEX0_1 = (seconds_h << 4) | seconds_l;
 }
 
 void test_gps() {
--- gps.h
+++ gps.h
@@ -1,12 +1,15 @@
 #ifndef GPS_H_
 #define GPS_H_
 
-// Define bluetooth control registers
+// Define gps control registers
 #define GPS_STATUS   (*(volatile unsigned char *)(0x84000210))
 #define GPS_CONTROL  (*(volatile unsigned char *)(0x84000210))
 #define GPS_TXDATA   (*(volatile unsigned char *)(0x84000212))
 #define GPS_RXDATA   (*(volatile unsigned char *)(0x84000212))
 #define GPS_BAUD     (*(volatile unsigned char *)(0x84000214))
+#define HEX0_1       (volatile unsigned char *) (0x2030)
+#define HEX2_3		 (volatile unsigned char *) (0x2040)
+#define HEX4_5 	     (volatile unsigned char *) (0x2050)
 
 #define GPS_TX_MASK 0x02
 #define GPS_RX_MASK 0x01
@@ -21,5 +24,6 @@ float convert_latitude(int x);
 float convert_longitude(int x);
 void utc_to_local(void);
 void test_gps(void);
+
 
 #endif /* GPS_H_ */
