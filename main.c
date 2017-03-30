2b7de42c85da7ed90b4971a2f868489d171f158a
Bruno Bachmann <bfbachmann@gmail.com>
2017-03-29 19:57:08 -0700

    Add bluetooth command response

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <time.h>
 #include <unistd.h>
+#include <pthread.h>
 #include "nios_system.h"
 #include "touch.h"
 #include "bluetooth.h"
@@ -17,17 +18,68 @@
 #include "distance.h"
 #include "altera_up_avalon_character_lcd.h"
 
-#define ONE_SECOND 				1000
 #define CAR_COUNT_INTERVAL		10
+#define TRUE 					1
+#define FALSE 					0
+
+int streaming = FALSE;
+clock_t stream_start;
+
+
+static void do_bluetooth(float *base_dist, struct gps_packet *gps_pkt) {
+	char bluetooth_msg[50];
+	char char_received = get_char();
+	float dist;
+
+	switch (char_received) {
+		case 'D':
+			streaming = TRUE;
+			stream_start = clock();
+			break;
+
+		case 'C':
+			streaming = FALSE;
+			base_dist = HEX0 + HEX1*10 + HEX2*100;
+			sprintf(bluetooth_msg, "$C%f\n", base_dist);
+			sendstring_bluetooth(bluetooth_msg);
+			break;
+
+		case 'S':
+			streaming = FALSE;
+			dist = HEX0 + HEX1*10 + HEX2*100;
+			update_gps_data(gps_pkt);
+			sprintf(bluetooth_msg, "$S%f,Connected,%f,%s,%s\n",
+				dist, base_dist, gps_pkt->latitude, gps_pkt->longitude);
+			sendstring_bluetooth(bluetooth_msg);
+			break;
+
+		case 'X':
+			streaming = FALSE;
+			break;
+
+		default: return;
+	}
+
+	if (streaming && (clock() - stream_start)/CLOCKS_PER_SEC > 1) {
+		stream_start = clock();
+		dist = HEX0 + HEX1*10 + HEX2*100;
+		sprintf(bluetooth_msg, "$D%f\n", dist);
+		sendstring_bluetooth(bluetooth_msg);
+	}
+
+	printf("Received command: %c\n", char_received);
+	printf("Sent reply: %s\n", bluetooth_msg);
+}
 
 
 int main() {
-	int car_count, x, y;
-	char char_received, bluetooth_msg[50];
-	float base_dist = HEX0 + HEX1*10 + HEX2*100;//read_dist();
+	int car_count, x, y, err;
+	float base_dist = HEX0 + HEX1*10 + HEX2*100;
 	Point point_touched;
+	struct gps_packet *gps_pkt;
 
 	printf("Initializing...\n");
+
 	init_gps();
 	init_wifi();
 	init_touch();
@@ -35,42 +87,40 @@ int main() {
 	startup_screen();
 	main_menu();
 	srand(time(NULL));
+
+	gps_pkt = gps_packet_create();
+
 	printf("Done Initializing\n");
 
 	while (1) {
-		point_touched = get_press();
-		x = point_touched.x;
-		y = point_touched.y;
+		if (screen_touched()) {
+			point_touched = get_press();
 
-		if (GPS_BUTTON) {
-			get_release();
-			display_gps();
+			x = point_touched.x;
+			y = point_touched.y;
+
+			if (GPS_BUTTON) {
+				get_release();
+				display_gps();
+			}
+
+			else if (CALIBRATE_BUTTON) {
+				get_release();
+				calibrate(&base_dist);
+			}
+
+			else if (CAR_BUTTON) {
+				get_release();
+				count_cars(CAR_COUNT_INTERVAL, base_dist, SOFTWARE_COUNTER);
+			}
+
+			else if (PLOT_BUTTON){
+				get_release();
+				plot_data(base_dist);
+			}
 		}
 
-		else if (CALIBRATE_BUTTON) {
-			get_release();
-			calibrate(&base_dist);
-		}
-
-		else if (CAR_BUTTON) {
-			get_release();
-			count_cars(CAR_COUNT_INTERVAL, base_dist, SOFTWARE_COUNTER);
-		}
-
-		else if (PLOT_BUTTON){
-			get_release();
-			plot_data(base_dist);
-		}
-
-		char_received = get_char();
-
-		if (char_received == 'C') {
-			calibrate(&base_dist);
-			printf("Received command: %c\n", char_received);
-			sprintf(bluetooth_msg, "$C%f\n\0", base_dist);
-			printf("Sending reply: %s\n", bluetooth_msg);
-			sendstring_bluetooth(bluetooth_msg);
-		}
+		do_bluetooth(&base_dist, gps_pkt);
 	}
 
 	return 0;
